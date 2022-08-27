3b6e92b33e90d8d0507c023738fdb602da6a577e
nani-samireddy <nanisamireddy05@gmail.com>
2022-08-27 09:36:33 +0530

    fixed the next button links problem

--- public/Languages/Telugu/nextAndPreviousSongScript.js
+++ public/Languages/Telugu/nextAndPreviousSongScript.js
@@ -7,15 +7,14 @@ const currentSongId = document.getElementById('songId').textContent;
 
 
 
-function matchSongId(song) {
-    return song.iD = currentSongId;
-}
-
 function addSongsLinks() {
 
-    let index = indexList.findIndex(matchSongId)
-    let previousSongLink = index == 0 ? indexList[index].link : indexList[index - 1].link;
-    let nextsongLink = index == indexList.length - 1 ? indexList[index].link : indexList[index + 1].link;
+    let index = indexList.findIndex(function (song) {
+        return song.iD === currentSongId.trim();
+    })
+    console.log(index);
+    let previousSongLink = (index == 0) ? indexList[index].link : indexList[index].link;
+    let nextsongLink = index == (indexList.length - 1) ? indexList[index].link : indexList[index + 1].link;
     previousSongLinkAnchor.href = previousSongLink;
     nextSongLinkAnchor.href = nextsongLink;
     console.log('Added next and previous songs links');
