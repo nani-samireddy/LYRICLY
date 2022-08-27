1e7207465fefcadc8ca260e569c3bb51116e4cd7
nani-samireddy <nanisamireddy05@gmail.com>
2022-08-27 11:04:42 +0530

    fix the index loading time by pagination with scroll trigger

--- public/Languages/Telugu/mainIndexManuplation.js
+++ public/Languages/Telugu/mainIndexManuplation.js
@@ -1,17 +1,42 @@
 import { indexList } from "./songsIndex.js";
 
 const mainIndexContainer = document.getElementById("IndexContainer");
+const loadMoreMessage = document.getElementById('loadingMore');
+
+let index = 0;
+const limit = 500;
 
 
 function LoadMainIndex() {
     let linkPrefix = window.location.href.replace("TeluguIndex.html", "") + "songs/";
-    indexList.forEach(song => {
+
+    for (let count = 0; index < indexList.length && count < limit; index++, count++) {
+        const song = indexList[index];
         let link = linkPrefix + song.link
         let element = `<li class="cursor-pointer pt-3 pb-2 pl-3 mb-2 rounded-md text-black text-xl hover:underline"><a href="${link}"> <span class="pl-5"> ${song.title}</span></a></li>`;
         mainIndexContainer.innerHTML = mainIndexContainer.innerHTML + element;
-    });
+
+    }
+
+
+    // indexList.forEach(song => {
+    //     let link = linkPrefix + song.link
+    //     let element = `<li class="cursor-pointer pt-3 pb-2 pl-3 mb-2 rounded-md text-black text-xl hover:underline"><a href="${link}"> <span class="pl-5"> ${song.title}</span></a></li>`;
+    //     mainIndexContainer.innerHTML = mainIndexContainer.innerHTML + element;
+    // });
 
 }
 
+window.onscroll = function (ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
+        if (index < indexList.length) {
+            LoadMainIndex();
+        } else {
+            loadMoreMessage.classList.remove('block');
+            loadMoreMessage.classList.add('hidden');
+
+        }
+    }
+};
 
 LoadMainIndex()
